72222482eda051fccceb8e6f5f03e614de50e45c
tefloon <tefloon@gmail.com>
2023-11-15 12:22:38 +0100

    Scroll down button. Refactoring.

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function Chat() {
   return (
     <WebSocketProvider>
-      <h1>Chat Page Component</h1>
+      <h1 className="text-center py-10 text-2xl">Chat Page Component</h1>
       <div className="flex flex-col items-center">
         <ChatWindow />
       </div>
--- src/components/webSocketComponents/chatComponents/chatWindow.tsx
+++ src/components/webSocketComponents/chatComponents/chatWindow.tsx
@@ -9,7 +9,7 @@ import { flushSync } from "react-dom";
 
 export default function ChatWindow() {
   const DEFAULT_MAX_VISIBLE_MESSAGES = 20;
-  const MAX_MESSAGES_CAP = 100;
+  const MAX_MESSAGES_CAP = 40;
 
   const { send, onMessage } = useWebSocketContext();
 
@@ -17,7 +17,11 @@ export default function ChatWindow() {
     DEFAULT_MAX_VISIBLE_MESSAGES
   );
   const [chatMessages, setChatMessages] = useState<ChatMessageType[]>([]);
+
   const [currentScrollTop, setCurrentScrollTop] = useState(0);
+  const [clientHeight, setClientHeight] = useState(0);
+  const [scrollHeight, setScrollHeight] = useState(0);
+
   const [canLoadOlder, setCanLoadOlder] = useState(true);
 
   const listRef = useRef<HTMLUListElement>(null);
@@ -26,6 +30,8 @@ export default function ChatWindow() {
   const chatMessagesRef = useRef(chatMessages);
   const visibleMessagesRef = useRef(visibleMessages);
   const canLoadOlderRef = useRef(canLoadOlder);
+  const hasScrolled = useRef(false);
+  const isAutoScroll = useRef(true);
 
   const [userName, setUsername] = useState("");
 
@@ -42,7 +48,14 @@ export default function ChatWindow() {
   useEffect(() => {
     const handleScroll = () => {
       if (containerRef.current) {
+        if (isAutoScroll.current) {
+          isAutoScroll.current = false;
+        } else {
+          hasScrolled.current = true;
+        }
         setCurrentScrollTop(containerRef.current.scrollTop);
+        setClientHeight(containerRef.current.clientHeight);
+        setScrollHeight(containerRef.current.scrollHeight);
         loadOlderMessages();
       }
     };
@@ -79,7 +92,7 @@ export default function ChatWindow() {
       flushSync(() => {
         setChatMessages((prevMessages) =>
           prevMessages
-            ? [...[...prevMessages, newMessage].slice(-MAX_MESSAGES_CAP)]
+            ? [...[...prevMessages.slice(-MAX_MESSAGES_CAP + 1), newMessage]]
             : [newMessage]
         );
       });
@@ -114,35 +127,50 @@ export default function ChatWindow() {
   //   FUNCTIONS  //
   // ============ //
 
-  const scrollToLastMessage = () => {
-    if (!listRef.current) return;
+  const isAtBottom = (threshold: number) => {
     if (!containerRef.current) return;
-    const scrollThreshold = 150;
-    // Define a threshold (e.g., 150px) to consider user is at the bottom
     const { scrollTop, clientHeight, scrollHeight } = containerRef.current;
+    const isAtBottom = scrollTop + clientHeight + threshold >= scrollHeight;
 
-    const isAtBottom =
-      scrollTop + clientHeight + scrollThreshold >= scrollHeight;
+    return isAtBottom;
+  };
 
-    if (isAtBottom) {
-      let lastChild = listRef.current.lastElementChild;
-      lastChild?.scrollIntoView({
-        behavior: "smooth",
-        block: "end",
-        inline: "nearest",
-      });
+  const scrollToLastMessage = (forced = false) => {
+    const scrollThreshold = 50;
+
+    if (!listRef.current) return;
+
+    const lastMessage = listRef.current.lastElementChild;
+    let lastMessageHeight = 0;
+
+    if (lastMessage && lastMessage instanceof HTMLLIElement) {
+      lastMessageHeight = lastMessage.offsetHeight;
+
+      if (
+        isAtBottom(lastMessageHeight + scrollThreshold) ||
+        !hasScrolled ||
+        forced
+      ) {
+        isAutoScroll.current = true;
+
+        lastMessage.scrollIntoView({
+          behavior: "instant",
+          block: "end",
+          inline: "nearest",
+        });
+      }
     }
   };
 
   const loadOlderMessages = () => {
     if (!canLoadOlderRef.current) return;
     if (!visibleMessagesRef.current) return;
-    if (!listRef.current || !containerRef.current) return;
+    if (!containerRef.current) return;
 
     if (chatMessagesRef.current.length < DEFAULT_MAX_VISIBLE_MESSAGES) return;
-    if (visibleMessagesRef.current > chatMessagesRef.current.length) return;
+    if (visibleMessagesRef.current >= chatMessagesRef.current.length) return;
 
-    const scrollThreshold = 150;
+    const scrollThreshold = 200;
     // Define a threshold (e.g., 150px) to consider user is at the bottom
     const { scrollTop, scrollHeight } = containerRef.current;
 
@@ -154,8 +182,16 @@ export default function ChatWindow() {
     }
   };
 
+  const scrollBtnBaseClass =
+    "absolute bottom-40 left-1/2 -translate-x-1/2 px-5 py-3 bg-slate-900/25 hover:bg-slate-900/75 rounded-full";
+
+  let scrollBtnClass =
+    currentScrollTop < scrollHeight - clientHeight - 100
+      ? scrollBtnBaseClass
+      : "hidden";
+
   return (
-    <div className="flex flex-col h-[800px] w-1/3 gap-5">
+    <div className="flex flex-col h-[800px] w-1/3 gap-5 relative">
       <div
         className="h-full overflow-y-auto overflow-x-hidden scroll-smooth"
         ref={containerRef}
@@ -171,13 +207,19 @@ export default function ChatWindow() {
           </ul>
         )}
       </div>
+      <button
+        className={scrollBtnClass}
+        onClick={() => scrollToLastMessage(true)}
+      >
+        Scroll Down
+      </button>
       <div>
         {/* Sending form */}
+        {/* TODO: Extract form to a separate component */}
         <form className="flex flex-row" onSubmit={handleSendMessage}>
           <button className="flex-none bg-slate-900 text-slate-400 p-5 ">
             Send Message
           </button>
-
           <input
             className="grow text-xl text-slate-900 bg-slate-400 p-4"
             type="text"
@@ -188,7 +230,10 @@ export default function ChatWindow() {
         </form>
       </div>
       <div className="flex flex-row justify-between">
-        <span>{currentScrollTop}</span>
+        <span>
+          {currentScrollTop} + {clientHeight} = {scrollHeight}
+        </span>
+        <span>{hasScrolled.current.toString()}</span>
         <span>
           {visibleMessages} / {chatMessages.length}
         </span>
